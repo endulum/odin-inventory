705aaab2c54f15c9e3ed3a0c8e4868e0576ef1b9
endulum <71853916+endulum@users.noreply.github.com>
2023-12-04 12:52:36 -0800

    can create and update categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const Part = require('../models/part');
 const Category = require('../models/category');
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
   const allCategories = await Category.find({}).exec();
@@ -12,6 +13,67 @@ exports.category_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
+  let thisCategory, allPartsInCategory
+  try {
+    thisCategory = await Category.findById(req.params.id).populate('items').exec();
+    allPartsInCategory = await Part.find({ category: req.params.id }).exec();
+  } catch {
+    thisCategory = null;
+    allPartsInCategory = [];
+  }
+
+  if (thisCategory === null) {
+    const err = new Error('Category not found.');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('category/detail', {
+    title: `Viewing Category: ${thisCategory.name}`,
+    category: thisCategory,
+    parts: allPartsInCategory 
+  })
+});
+
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+  res.render('category/form', { title: 'New Category' });
+});
+
+exports.category_create_post = [
+  body('category_name', 'Category must have a name.')
+    .trim()
+    .isLength({ min: 1, max: 32 })
+    .escape()
+    .withMessage('Category name cannot be longer than 32 characters.'),
+  
+  body('category_description', 'Category must have a description.')
+    .trim()
+    .isLength({ min: 1, max: 512 })
+    .escape()
+    .withMessage('Category description cannot be longer than 512 characters.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.category_name,
+      description: req.body.category_description
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('category/form', { 
+        title: 'New Category',
+        category: category,
+        errors: errors.array()
+      });
+    } else {
+      await category.save();
+      res.redirect(category.url);
+    }
+  })
+];
+
+exports.category_update_get = asyncHandler(async (req, res, next) => {
   let thisCategory;
   try {
     thisCategory = await Category.findById(req.params.id).populate('items').exec();
@@ -24,29 +86,47 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
     err.status = 404;
     return next(err);
   }
+  
+  res.render('category/form', { 
+    title: 'Update Category',
+    category: thisCategory
+  });
+});
 
-  res.render('category/detail', {
-    title: `Viewing Category: ${thisCategory.name}`,
-    category: thisCategory,
-    parts: thisCategory.items
+exports.category_update_post = [
+  body('category_name', 'Category must have a name.')
+    .trim()
+    .isLength({ min: 1, max: 32 })
+    .escape()
+    .withMessage('Category name cannot be longer than 32 characters.'),
+  
+  body('category_description', 'Category must have a description.')
+    .trim()
+    .isLength({ min: 1, max: 512 })
+    .escape()
+    .withMessage('Category description cannot be longer than 512 characters.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      _id: req.params.id,
+      name: req.body.category_name,
+      description: req.body.category_description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('category/form', { 
+        title: 'New Category',
+        category: category,
+        errors: errors.array()
+      });
+    } else {
+      await Category.findByIdAndUpdate(req.params.id, category);
+      res.redirect(category.url);
+    }
   })
-});
-
-exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send('form for making a new category');
-});
-
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send('should make the category and redirect to overview');
-});
-
-exports.category_update_get = asyncHandler(async (req, res, next) => {
-  res.send('form for updating a category');
-});
-
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-  res.send('should update the category and redirect to overview');
-});
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
   res.send('confirmation page for deleting a category');
