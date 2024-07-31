6d71a3905ba01f49440257b686599b27d72e5412
endulum <71853916+endulum@users.noreply.github.com>
2024-07-31 21:56:46 +0100

    add nicer error handling

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 require('dotenv').config()
 
 const express = require('express')
+const asyncHandler = require('express-async-handler')
 const logger = require('morgan')
 const queries = require('./db/queries')
 
 const app = express()
 app.use(logger('dev'))
 
-app.get('/parts', async (req, res) => {
+app.get('/parts', asyncHandler(async (req, res) => {
   const parts = await queries.getAllParts()
   console.log(parts)
   return res.sendStatus(200)
-})
+}))
 
-app.get('/parts/:id', async (req, res) => {
+app.get('/parts/:id', asyncHandler(async (req, res) => {
   const part = await queries.getPartById(req.params.id)
   console.log(part)
   if (!part) return res.sendStatus(404)
   return res.sendStatus(200)
-})
+}))
 
-app.get('/categories', async (req, res) => {
+app.get('/categories', asyncHandler(async (req, res) => {
   const categories = await queries.getAllCategories()
   console.log(categories)
   return res.sendStatus(200)
-})
+}))
 
-app.get('/categories/:id', async (req, res) => {
+app.get('/categories/:id', asyncHandler(async (req, res) => {
   const category = await queries.getCategoryById(req.params.id)
   console.log(category)
   if (!category) return res.sendStatus(404)
   return res.sendStatus(200)
+}))
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  return res.sendStatus(err.statusCode || 500)
 })
 
 const port = process.env.PORT || 3000
