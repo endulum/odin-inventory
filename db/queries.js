a5b928fed93e8cd7f3baed7c916e94bb537bd8f5
endulum <71853916+endulum@users.noreply.github.com>
2024-08-01 21:01:27 +0100

    add parts view with search functionality

--- db/queries.js
+++ db/queries.js
@@ -7,8 +7,27 @@ queries.getAllCategories = async function() {
   return rows
 }
 
-queries.getAllParts = async function() {
-  const { rows } = await pool.query('SELECT * FROM parts')
+queries.getAllParts = async function(reqQuery) {
+  const whereClauses = []
+  if ('name' in reqQuery && reqQuery.name !== '') {
+    whereClauses.push(`LOWER(parts.name) LIKE LOWER('%${reqQuery.name}%')`)
+  }
+  if ('category' in reqQuery && reqQuery.category !== '') {
+    whereClauses.push(`categories.name = '${reqQuery.category}'`)
+  }
+  if ('isInStock' in reqQuery && reqQuery.isInStock === 'on') {
+    whereClauses.push(`in_stock > 0`)
+  }
+  const whereString = whereClauses.join(` AND `)
+
+  const sql = `
+  SELECT parts.name AS name, categories.name AS category, price, in_stock
+    FROM parts JOIN categories 
+    ON (parts.category_id = categories.id)
+    ${whereString && whereString.length !== 0 ? `WHERE ${whereString}` : ''}
+  ;`
+
+  const { rows } = await pool.query(sql)
   return rows
 }
 
