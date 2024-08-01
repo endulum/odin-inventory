b0623df0007877d90dc73135f12c0585cd4cdbce
endulum <71853916+endulum@users.noreply.github.com>
2024-08-01 14:58:15 +0100

    switch to ejs and create views and public assets

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 require('dotenv').config()
 
+const path = require('path')
 const express = require('express')
-const asyncHandler = require('express-async-handler')
 const logger = require('morgan')
 const categoriesRouter = require('./routes/categoriesRouter')
 const partsRouter = require('./routes/partsRouter')
 
 const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req, res) => {
+  return res.render('layout', {
+    page: 'pages/index',
+    title: 'Index'
+  })
+})
 
 app.use('/categories', categoriesRouter)
 app.use('/parts', partsRouter)
